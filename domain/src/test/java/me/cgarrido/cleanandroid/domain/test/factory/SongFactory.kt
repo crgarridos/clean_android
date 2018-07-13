391800d8f73f0aa8cab19f170e71f4451891001f
Cristian Garrido <crgarrido.s@gmail.com>
2018-07-13 16:16:52 +0200

    makes makeList generate unique ids for songs

--- domain/src/test/java/me/cgarrido/cleanandroid/domain/test/factory/SongFactory.kt
+++ domain/src/test/java/me/cgarrido/cleanandroid/domain/test/factory/SongFactory.kt
@@ -4,12 +4,13 @@ import me.cgarrido.cleanandroid.domain.model.Song
 import me.cgarrido.cleanandroid.util.DataFactory
 
 object SongFactory {
-    fun makeSong(): Song = with(DataFactory) {
+    fun makeSong(id: Long = DataFactory.randomLong()): Song = with(DataFactory) {
         Song(randomLong(), randomLong(), randomString(), randomString(), randomString())
     }
 
     fun makeList(size: Int): List<Song> {
-        return generateSequence(::makeSong)
-                .take(size).toList()
+        return generateSequence(makeSong(1)) {
+            it.copy(id = it.id + 1)
+        }.take(size).toList()
     }
 }
