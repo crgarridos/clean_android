d9f96e9315ebf373b5d13520690a0ecd15238d9c
Cristian Garrido <crgarrido.s@gmail.com>
2018-07-13 17:20:42 +0200

    Single instance for picasso

--- app/src/main/java/me/cgarrido/cleanandroid/injection/module/PicassoModule.kt
+++ app/src/main/java/me/cgarrido/cleanandroid/injection/module/PicassoModule.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import com.squareup.picasso.Picasso
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 object PicassoModule {
 
     @Provides
     @JvmStatic
+    @Singleton
     fun providePicasso(context: Context): Picasso {
         return Picasso.Builder(context).build()
     }
